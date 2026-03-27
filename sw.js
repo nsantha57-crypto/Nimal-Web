08ad2af216034e470a18706e211774d8c6c9bf60
nsantha57-crypto <nsantha57@gmail.com>
2026-03-28 01:58:06 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nimal-web-cache-v1';
+const CACHE_NAME = 'nimal-web-cache-v3'; // Incremented version
 const urlsToCache = [
   './',
   './index.html',
@@ -9,18 +9,40 @@ const urlsToCache = [
   'https://unpkg.com/lucide@latest'
 ];
 
+// Install event - caching the assets
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Force the new service worker to become active immediately
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Activate event - cleaning up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Take control of all open clients immediately
+  );
+});
+
+// Fetch event - serving from cache or network
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        if (response) return response;
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
         return fetch(event.request);
       })
   );
